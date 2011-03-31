92b527658bd23f5b2c804318ebb493fb086fe34b
Robin Gareus <robin@gareus.org>
2011-03-31 17:07:38 +0200

    re-order test-scenario.

--- tests.sh
+++ tests.sh
@@ -12,31 +12,7 @@ if [ ! -f $WAV ]; then
 fi
 
 if true; then
-  ./jack-stdout -d 2 -e signed   -b  8    $INPORTS   | ./jack-stdin -e signed   -b  8    $OUTPORTS
-  ./jack-stdout -d 2 -e signed   -b  8 -B $INPORTS   | ./jack-stdin -e signed   -b  8 -B $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b  8    $INPORTS   | ./jack-stdin -e unsigned -b  8    $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b  8 -B $INPORTS   | ./jack-stdin -e unsigned -b  8 -B $OUTPORTS
-
-  ./jack-stdout -d 2 -e signed   -b 16    $INPORTS   | ./jack-stdin -e signed   -b 16    $OUTPORTS
-  ./jack-stdout -d 2 -e signed   -b 16 -B $INPORTS   | ./jack-stdin -e signed   -b 16 -B $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b 16    $INPORTS   | ./jack-stdin -e unsigned -b 16    $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b 16 -B $INPORTS   | ./jack-stdin -e unsigned -b 16 -B $OUTPORTS
-
-  ./jack-stdout -d 2 -e signed   -b 24    $INPORTS   | ./jack-stdin -e signed   -b 24    $OUTPORTS
-  ./jack-stdout -d 2 -e signed   -b 24 -B $INPORTS   | ./jack-stdin -e signed   -b 24 -B $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b 24    $INPORTS   | ./jack-stdin -e unsigned -b 24    $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b 24 -B $INPORTS   | ./jack-stdin -e unsigned -b 24 -B $OUTPORTS
-
-  ./jack-stdout -d 2 -e signed   -b 32    $INPORTS   | ./jack-stdin -e signed   -b 32    $OUTPORTS
-  ./jack-stdout -d 2 -e signed   -b 32 -B $INPORTS   | ./jack-stdin -e signed   -b 32 -B $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b 32    $INPORTS   | ./jack-stdin -e unsigned -b 32    $OUTPORTS
-  ./jack-stdout -d 2 -e unsigned -b 32 -B $INPORTS   | ./jack-stdin -e unsigned -b 32 -B $OUTPORTS
-
-  ./jack-stdout -d 2 -e float    -b 32    $INPORTS   | ./jack-stdin -e float    -b 32    $OUTPORTS
-  ./jack-stdout -d 2 -e float    -b 32 -B $INPORTS   | ./jack-stdin -e float    -b 32 -B $OUTPORTS
-fi
-
-if true; then
+	echo "testing formats: sox | ./jack-stdin"
   sox $WAV -t raw -r 48k -e signed   -b  8 -c 2    - | ./jack-stdin -e signed   -b 8     $OUTPORTS
   sox $WAV -t raw -r 48k -e signed   -b  8 -c 2 -B - | ./jack-stdin -e signed   -b 8 -B  $OUTPORTS
   sox $WAV -t raw -r 48k -e unsigned -b  8 -c 2    - | ./jack-stdin -e unsigned -b 8     $OUTPORTS
@@ -59,6 +35,32 @@ if true; then
 
   sox $WAV -t raw -r 48k -e float    -b 32 -c 2    - | ./jack-stdin -e float    -b 32    $OUTPORTS
   sox $WAV -t raw -r 48k -e float    -b 32 -c 2 -B - | ./jack-stdin -e float    -b 32 -B $OUTPORTS
+fi
+
+if true; then
+	echo "testing ./jack-stdout | ./jack-stdin"
+  ./jack-stdout -d 3 -e signed   -b  8    $INPORTS   | ./jack-stdin -e signed   -b  8    $OUTPORTS
+  ./jack-stdout -d 3 -e signed   -b  8 -B $INPORTS   | ./jack-stdin -e signed   -b  8 -B $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b  8    $INPORTS   | ./jack-stdin -e unsigned -b  8    $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b  8 -B $INPORTS   | ./jack-stdin -e unsigned -b  8 -B $OUTPORTS
+
+  ./jack-stdout -d 3 -e signed   -b 16    $INPORTS   | ./jack-stdin -e signed   -b 16    $OUTPORTS
+  ./jack-stdout -d 3 -e signed   -b 16 -B $INPORTS   | ./jack-stdin -e signed   -b 16 -B $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b 16    $INPORTS   | ./jack-stdin -e unsigned -b 16    $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b 16 -B $INPORTS   | ./jack-stdin -e unsigned -b 16 -B $OUTPORTS
+
+  ./jack-stdout -d 3 -e signed   -b 24    $INPORTS   | ./jack-stdin -e signed   -b 24    $OUTPORTS
+  ./jack-stdout -d 3 -e signed   -b 24 -B $INPORTS   | ./jack-stdin -e signed   -b 24 -B $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b 24    $INPORTS   | ./jack-stdin -e unsigned -b 24    $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b 24 -B $INPORTS   | ./jack-stdin -e unsigned -b 24 -B $OUTPORTS
+
+  ./jack-stdout -d 3 -e signed   -b 32    $INPORTS   | ./jack-stdin -e signed   -b 32    $OUTPORTS
+  ./jack-stdout -d 3 -e signed   -b 32 -B $INPORTS   | ./jack-stdin -e signed   -b 32 -B $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b 32    $INPORTS   | ./jack-stdin -e unsigned -b 32    $OUTPORTS
+  ./jack-stdout -d 3 -e unsigned -b 32 -B $INPORTS   | ./jack-stdin -e unsigned -b 32 -B $OUTPORTS
+
+  ./jack-stdout -d 3 -e float    -b 32    $INPORTS   | ./jack-stdin -e float    -b 32    $OUTPORTS
+  ./jack-stdout -d 3 -e float    -b 32 -B $INPORTS   | ./jack-stdin -e float    -b 32 -B $OUTPORTS
 fi
 
 test -n "$RM" && rm $WAV
